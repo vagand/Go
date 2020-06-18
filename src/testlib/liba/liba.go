6df8db996d857899fd9ee28914401273b2ca5aa7
vagand <rlehddnr@gmail.com>
2020-06-18 15:11:06 +0900

    Add package scope and member function

--- src/testlib/liba/liba.go
+++ src/testlib/liba/liba.go
@@ -5,6 +5,29 @@ import (
 	_ "testlib/libc"
 )
 
+type Show struct{}
+type hide struct{}
+
 func init() {
 	fmt.Println("liba.go init")
+}
+
+func (f Show) Foo() {
+	fmt.Println("Show.Foo")
+}
+
+func (f Show) foo() {
+	fmt.Println("Show.foo")
+}
+
+func (f Show) Foo2() {
+	f.foo()
+}
+
+func fhide() {
+	fmt.Println("fhide")
+}
+
+func Fshow() {
+	fmt.Println("Fshow")
 }
--- src/testlib/main.go
+++ src/testlib/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
-	_ "testlib/liba"
+	"testlib/liba"
 )
 
 func main() {
 	fmt.Println("main.go")
+
+	s := liba.Show{}
+	s.Foo()
+	s.Foo2()
+	liba.Fshow()
 }
